2f01c8ab0222fa3ebe6e6f020959cc86621d00d9
johannah <jh1736@gmail.com>
2019-09-12 13:58:23 -0400

    added using experiment data

--- prepare_output_data.py
+++ prepare_output_data.py
@@ -52,37 +52,54 @@ def load_training_files(exp_name):
     all_data.index = np.arange(all_data.shape[0])
     return all_data
 
-def get_images(datadir, labeled_data):
-    output_csv = os.path.join(datadir, 'output_summary.csv')
+def get_images(datadir, labeled_data, exp_data):
+    output_csv = os.path.join(datadir, 'output_summary_class.csv')
     if not os.path.exists(output_csv):
         # we know the label for these images
         labeled_data.index = np.arange(labeled_data.shape[0])
         labeled_jpgs = [os.path.join(datadir, labeled_data.loc[i,'exp_name'], labeled_data.loc[i, 'sub_exp_name'], labeled_data.loc[i, 'img_file_name']) for i in range(labeled_data.shape[0])]
 
+        exp_files = list(exp_data.loc[:,'file'])
         jpgs = sorted(glob(os.path.join(datadir, '*/*/*.jpg')))
         output = []
         ll_cnt = 0
+        exp_cnt = 0
         ul_cnt = 0
         for jpg in jpgs:
             if jpg in labeled_jpgs:
                 index = labeled_jpgs.index(jpg)
+                label = labeled_data.loc[index, 'object_annotation_category'].lower()
+                # dependent on the data that the network was trained on
+                if jpg in exp_files:
+                    exp_index = exp_files.index(jpg)
+                    exp_class = exp_data.loc[exp_index,'class_label'].lower()
+                    exp_files.pop(exp_index)
+                    exp_cnt +=1
+                else:
+                    exp_index = -1
+                    exp_class = 'none'
                 row = labeled_data.loc[index]
-                output.append([jpg, row['object_annotation_category'].lower(), index])
+                # TODO - add class label
+                output.append([jpg,  exp_class, label, exp_index, index])
                 ll_cnt +=1
                 # reduce search 
                 labeled_jpgs.pop(index)
             else:
                 ul_cnt +=1
-                output.append([jpg, 'unk', 'unk']) 
+                output.append([jpg, 'unk', 'unk', 'unk', 'unk']) 
 
-        outpd = pd.DataFrame(np.array(output), columns=['file', 'label', 'summary_index'])
+        print(exp_cnt, ll_cnt)
+        outpd = pd.DataFrame(np.array(output), columns=['file', 'class_label', 'label', 'experiment_index', 'summary_index'])
         outpd.to_csv(output_csv, sep=',')
+        embed()
     else:
         outpd = pd.read_csv(output_csv)
     return outpd
 
 if __name__ == '__main__':
     datadir = './data/UVP_data_folder/'
+    exp_name = 'uvp_big_1000small_noliving_rotate_bg0_trim_combine'
+    exp_data = load_training_files(exp_name)
     labeled_data = load_dataset_files(datadir)
-    all_image_paths = get_images(datadir, labeled_data)
+    all_image_paths = get_images(datadir, labeled_data, exp_data)
 
